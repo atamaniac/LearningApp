d0faa820e69ee2cd4d09830af207fc35743132b6
atamaniac <38379857+atamaniac@users.noreply.github.com>
2020-04-21 20:50:29 +0200

    makeshift table

--- LearningApp/flaskApp/forms.py
+++ LearningApp/flaskApp/forms.py
@@ -5,6 +5,7 @@ from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flaskApp.models import User
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
     email = StringField('Email',validators=[DataRequired(),Email()])
